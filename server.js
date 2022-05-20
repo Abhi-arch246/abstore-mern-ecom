124fc4afcee9e160b9396feba0986f50c4809072
Abhi-arch246 <abhishekreddy235@gmail.com>
2022-05-20 14:28:35 +0530

    Deploy lines edited

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-var dbconnection = require('./dbconnect')
+const path = require('path')
 var UserRoute = require('./routes/UserRoute')
 var ProductRoute = require('./routes/ProductRoute')
 var OrderRoute = require('./routes/OrderRoute')
@@ -9,9 +9,13 @@ app.use(bodyParser.json())
 app.use("/api/products/", ProductRoute)
 app.use("/api/users/", UserRoute)
 app.use("/api/orders/", OrderRoute)
-app.get("/", (req, res) => {
-    res.send("Response is working")
-})
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log("Server started on server 5000"))
